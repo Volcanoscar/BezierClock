d20bc5d29a93d37a07480484c50022c5923e0806
Oleg Godovykh <olegg.lieangel@gmail.com>
2015-01-08 00:47:39 -0500

    add stubs for configuration activities

---
+++ wallpaper/src/main/java/net/olegg/bezierclock/watchface/BezierWatchCompanionActivity.java
@@ -0,0 +1,9 @@
+package net.olegg.bezierclock.watchface;
+
+import android.app.Activity;
+
+/**
+ * Created by olegg on 08.01.15.
+ */
+public class BezierWatchCompanionActivity extends Activity {
+}
---
+++ watchface/src/main/java/net/olegg/bezierclock/watchface/BezierWatchConfigActivity.java
@@ -0,0 +1,14 @@
+package net.olegg.bezierclock.watchface;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by olegg on 07.01.15.
+ */
+public class BezierWatchConfigActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
